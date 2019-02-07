b15361f5d050be1ddfbcead57dd0446e85984ce3
jweezy24 <Jackwest245@gmail.com>
2019-02-07 12:20:45 -0600

    added some structuring and started developing a deviceManager class

--- src/local/deviceManager.py
+++ src/local/deviceManager.py
@@ -1,3 +1,7 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto('hello'.encode(), ('67.163.37.156', 7999))
+class deviceManager:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
