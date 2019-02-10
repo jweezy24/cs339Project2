3506a3d9f48d41a5eab39e2b25d71f928d5d32ed
jweezy <jwest1@luc.edu>
2019-02-10 15:11:50 -0600

    fixes

--- src/local/deviceManager.py
+++ src/local/deviceManager.py
@@ -1,26 +1,28 @@
 import socket
 import netifaces
+import sys
 
 class deviceManager:
     def __init__(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind(('', 8000))
+        self.server_address = '67.163.37.156'
         self.objects = []
     def listen(self):
         message, address = self.server_socket.recvfrom(1024)
+        sys.exit(0)
+        self.clapBack(address)
         try:
             json_message = eval(message)
             subnet_mask = self.getMask()
             for i in subnet_mask.keys():
-                print(subnet_mask.get(i)[1])
-                print(json_message['ip'])
                 if subnet_mask.get(i)[1] == json_message['ip']:
                     json_message['sub'] = subnet_mask.get(i)[0]
             if json_message['op'] == 'add':
                 print('object added.')
                 self.objects.append(json_message)
-                self.client_socket.sendto(str(json_message).encode(), ('localhost', 7999))
+                self.client_socket.sendto(str(json_message).encode(), (self.server_address, 7999))
         except NameError:
             print('Incorrect Json format')
     def getMask(self):
@@ -31,6 +33,8 @@ class deviceManager:
             if len(tempDict.keys()) > 0:
                 addresses.update({i: (tempDict.get(2)[0]['netmask'], tempDict.get(2)[0]['addr'])})
         return addresses
+    def clapBack(self,address):
+        self.server_socket.sendto("aye".encode(), address)
 
 
 def main():
