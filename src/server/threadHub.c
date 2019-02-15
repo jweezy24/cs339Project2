dbc2f1a4e4589fbcd43701ba23b4b2451fa995a0
jweezy24 <Jackwest245@gmail.com>
2019-02-15 10:56:16 -0600

    constantly sending and recieving.

--- src/server/threadHub.c
+++ src/server/threadHub.c
@@ -8,6 +8,6 @@ void create_recieve_thread(char* port){
   void* ret;
 
   pthread_create(&tid, NULL, recieve_packet, (void *)port);
-  
-  pthread_exit(NULL);
+  pthread_join(tid, &ret);
+  //pthread_exit(NULL);
 }
