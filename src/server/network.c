bcb0a7c40d2067d344363cb43450a11f0dee75f7
jweezy24 <Jackwest245@gmail.com>
2019-02-18 10:13:52 -0600

    merged and corrected errors after merge for testing

--- src/server/network.c
+++ src/server/network.c
@@ -50,32 +50,21 @@ void update_DM_on_net(DM* dm, char* ip, network* net){
 }
 
 void add_DM_to_net(DM dm, network* net){
-<<<<<<< HEAD
   printf("%d\n", net->size);
   net->things = (DM*)realloc(net->things, sizeof(DM)*(net->size+1));
-=======
-  net->things = realloc(net->things, sizeof(DM)*(net->size+1));
->>>>>>> 22d0dd9d3a2eed4ff37eb2418826e5e794eb7a81
   net->things[net->size] = dm;
   net->size+=1;
 }
 void add_DM_to_net_new(DM dm, network* net){
-  net->things = malloc(sizeof(DM));
+  net->things = (DM*)malloc(sizeof(DM));
   net->things[0] = dm;
   net->size +=1;
 }
 
-<<<<<<< HEAD
-network* remove_from_network(network* tmpNet, DM* badDM){
-  network *newNet = (network*)malloc(sizeof(network));
-  newNet->things = (DM*)malloc(sizeof(DM));
-  newNet->size = 0;
-=======
 network remove_from_network(network* tmpNet, DM* badDM){
   network newNet;
   init_network(&newNet);
   newNet.size = 0;
->>>>>>> 22d0dd9d3a2eed4ff37eb2418826e5e794eb7a81
   int netPos=0;
   printf("NAME OF BAD DM: %s", badDM->ip);
   for(int i = 0; i < tmpNet->size; i++){
